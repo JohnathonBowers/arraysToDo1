68e9f67064f3729a87fdb2c5cc376a7dd21875d5
Johnathon Bowers <johnathonbowers@gmail.com>
2023-04-04 17:02:11 -0400

    added removeAt function

--- script.js
+++ script.js
@@ -58,3 +58,25 @@ function insertAt(arr, idx, val) {
     arr[idx] = val;
     return arr;
 }
+
+
+/*
+BONUS: Remove At
+
+Given an array and an index into array, remove and return the array value at that index. Prove the value is removed from the array by printing it. Think of popFront(arr) as equivalent to removeAt(arr,0).
+
+Examples:
+
+removeAt([1000,3,204,77], 1) => 3 returned, with [1000,204,77] printed in the function
+removeAt([8,20,55,44,98], 3) => 44 returned, with [8,20,55,98] printed in the function
+*/
+
+function removeAt(arr, idx) {
+    const indexVal = arr[idx];
+    for(let i = idx; i < arr.length - 1; i++){
+        arr[i] = arr[i + 1];
+    }
+    arr.length = arr.length - 1;
+    console.log(arr);
+    return indexVal;
+}
